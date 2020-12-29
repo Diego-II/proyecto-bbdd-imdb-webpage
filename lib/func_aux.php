55c9b4614a55a3c8b3640f3e741d279db10d3a80
Diego-II <43184602+dirarra@users.noreply.github.com>
2020-12-29 02:44:33 -0300

    intento de buscador

--- lib/func_aux.php
+++ lib/func_aux.php
@@ -1,5 +1,17 @@
 <?php
 
 function buscarPorNombre($db, $nombre){
-    $
+    // SELECT id FROM groups WHERE name ILIKE 'Administrator'
+    if (!pg_connection_busy($db)) {
+        pg_send_prepare($db, "my_query", 'SELECT title, year, rating FROM cc3201.movie WHERE name ILIKE $1');
+        $res1 = pg_get_result($db);
+      }
+    
+      // Execute the prepared query.  Note that it is not necessary to escape
+      // the string "Joe's Widgets" in any way
+      if (!pg_connection_busy($db)) {
+        pg_send_execute($db, "my_query", array("Joe's Widgets"));
+        $res2 = pg_fetch_all(pg_get_result($db));
+      }
+      return $res2;    
 }
